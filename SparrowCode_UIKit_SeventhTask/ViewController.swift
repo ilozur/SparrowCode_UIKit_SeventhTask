70ff5cbf4ac0375021c9888214df5c77f37a95ec
Edmond <edpodlegaev@yandex.ru>
2024-04-21 18:48:17 +0300

    Complete Task

--- SparrowCode_UIKit_SeventhTask/ViewController.swift
+++ SparrowCode_UIKit_SeventhTask/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    let imageHeight: CGFloat = 270
+
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: view.frame)
+        
+        scrollView.contentSize.height = view.frame.height * 2
+        scrollView.delegate = self
+
+        return scrollView
+    }()
+
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: .init(origin: .zero, size: .init(width: view.frame.width, height: imageHeight)))
+        
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
     }
 
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        if (offset <= .zero) {
+            imageView.frame.origin.y = offset
+            imageView.frame.size.height = imageHeight - offset
+        }
+        scrollView.verticalScrollIndicatorInsets.top = imageHeight - offset - view.safeAreaInsets.top
+    }
 }
-
